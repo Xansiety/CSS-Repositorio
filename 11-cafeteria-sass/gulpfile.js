47c38b58ab559a19bf507412e4d38fba7089b80b
xansiety <ferando543@outlook.com>
2022-12-22 18:06:41 -0600

    Se añaden tareas por defecto(series, paralell) con sass

--- 11-cafeteria-sass/gulpfile.js
+++ 11-cafeteria-sass/gulpfile.js
@@ -1,12 +1,16 @@
 
-const { src, dest , watch} = require('gulp');
+const { src, dest , watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
+// En sass({ outputStyle: 'compressed' }) para que el css se minifique
 function css(done) {
     //compilar sass
     //Pasos: 1 identificar el archivo sass, 2 compilarlo, 3 guardar el css
     src('src/scss/app.scss') //1
         .pipe( sass() ) //2
+        .pipe( postcss([autoprefixer()]) ) //2
         .pipe( dest('build/css') ); //3
 
         done();
@@ -17,5 +21,17 @@ function dev() {
     watch('src/scss/app.scss', css); //(archivo a vigilar, función a ejecutar)
 }
 
+// se llaman solo ejecutando desde la terminal "gulp"
+function tareaDefault(done) {
+    console.log('Soy la tarea por default');
+    done();
+}
+
 exports.css = css;
 exports.dev = dev;
+// Default task
+// exports.default = tareaDefault;
+// Series -> Se inicia una tarea, cuando termina se inicia la siguiente
+exports.default = series(css, dev);
+// Parallel -> Se inician las tareas al mismo tiempo
+// exports.default = parallel(css, dev);
